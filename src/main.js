c0fda0ed2bb447f0b7ae2483b479381deb30fb22
Silvan Schalkowski <147550631+navlisData@users.noreply.github.com>
2023-12-22 14:03:42 +0100

    Added: - Vuetify dependency
    Changed:
    - Replaced favicon

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Begin vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+//end vuetify
+
+
+createApp(App).use(vuetify).mount('#app')
